718793f2302d8708fdc72021b7af7ae26cb2630d
DreamHealer <swehtc@gmail.com>
2023-12-20 15:39:18 +0100

    workshopfix.pl: added @maps_recent
    added @maps_recent to make sure we avoid getting one of the 5 recently played maps as nextmap

--- workshopfix.pl
+++ workshopfix.pl
@@ -31,6 +31,8 @@ my $sleep_time = 1;             # Sleep time in seconds between checks
 my $fetch_maps_time = 1800;     # Interval to fetch map list (every half hour)
 
 my @maps;                       # Array to store available maps
+my @maps_recent;                # Array to store recently played maps
+my $max_recent_maps = 5;        # Max entries for @maps_recent
 my $collecting_maps = 0;        # Flag to track if currently collecting maps
 my $game_over_processed = 0;    # Flag to track if "Game Over" has been processed
 
@@ -82,6 +84,36 @@ sub trigger_map_list {
     $collecting_maps = 1;
 }
 
+# Add a map to maps_recent
+sub add_recent_map {
+    my ($map) = @_;
+    
+    # Add current map to @maps_recent unless already there
+    push @maps_recent, $map unless grep { $_ eq $map } @maps_recent;
+    printdt("Added map: $map to recently played maps list");
+    
+    # Check if the number of recent maps exceeds the limit
+    if (scalar @maps_recent > $max_recent_maps) {
+	my $oldest_map = shift @maps_recent; # Remove the oldest map
+	printdt("Removed oldest map: $oldest_map from recently played maps list");
+    }
+}
+
+# Get a random map except for recently played ones
+sub get_random_map_except_recent {
+    # Filter out recent maps from @maps
+    my @filtered_maps = grep { my $map = $_; not grep { $map eq $_ } @maps_recent } @maps;
+    
+    # Check if there are any maps left after filtering
+    if (@filtered_maps) {
+	# Return a random map from the filtered list
+	return $filtered_maps[rand @filtered_maps];
+    } else {
+	# Fallback: return a random map from the full list
+	return $maps[rand @maps];
+    }
+}
+
 # Parses each line read from the log
 sub parse_line {
     my ($line) = @_;
@@ -115,9 +147,12 @@ sub parse_line {
     # if ($line =~ /Game Over/ && !$game_over_processed) {
     if ($line =~ /^L \d{2}\/\d{2}\/\d{4} - \d{2}:\d{2}:\d{2}: Game Over:/ && !$game_over_processed) {
         $game_over_processed = 1;
-        my $nextmap = $maps[rand @maps];
+        my $nextmap = get_random_map_except_recent();
 
         if ($nextmap) {
+	    # Add next map to maps_recent unless already there
+	    add_recent_map ( $nextmap );
+	    
             # Announce and change to the next map
             system("timeout 3 ".$basedir."/cs2server send \"say Nextmap: $nextmap\"");
             printdt ("5 - Announced nextmap: $nextmap");
@@ -133,6 +168,11 @@ sub parse_line {
         } else {
             printdt ("7 - Error: Nextmap not selected. Maps might be empty.");
         }
+    } elsif ($line =~ /loaded spawngroup\(\s*\d+\)\s*:\s*SV:\s*\[\d+:\s*([\w_]+)\s*\|/) {
+	my $current_map = $1;
+	
+	# Add current map to maps_recent unless already there
+	add_recent_map ( $current_map );
     }
 }
 
@@ -165,6 +205,7 @@ while (1) {
     # Trigger map list update periodically
     if ($tick > $fetch_maps_time) {
 	trigger_map_list();
+	system("timeout 3 ".$basedir."/cs2server send \"status\"");
 	$tick = 0;
     }
     
